10b3ef1a321ba5b25dffc9cda3cbf90f1ddd3fae
dushyant60 <dushyantsom60@gmail.com>
2025-06-03 15:38:55 +0530

    Check karlete hai ek aur baar

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Lets Try Something New"
+    return "🚀 Lets Try Something New but go nahi raha hai"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
